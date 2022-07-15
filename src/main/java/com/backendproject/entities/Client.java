203b4ccec40e64117421bb329f6e6aa4a09faeb6
Matheus Gomes <mathe.gomes.silva@gmail.com>
2022-07-15 17:25:46 -0300

    Entity adjusted using lombock

--- src/main/java/com/backendproject/entities/Client.java
+++ src/main/java/com/backendproject/entities/Client.java
@@ -1,82 +1,29 @@
 package com.backendproject.entities;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
 @Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Client {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    long id;
-    String name;
-    int age;
-    String email;
+    private long id;
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String firstName;
 
-    public Client() {
+    private String lastName;
 
-    }
+    private int age;
 
-    public Client(String name, int age, String email) {
-        this.name = name;
-        this.age = age;
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client client = (Client) o;
-        return id == client.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                ", email='" + email + '\'' +
-                '}';
-    }
+    private String email;
 }
